2ae93e60aed2cdd2dbfcba1db63da2c010a36876
czmarikaivanova <marikaivanova@seznam.cz>
2017-06-23 04:16:41 +0200

    input files ignored

--- src/Agent.java
+++ src/Agent.java
@@ -1,4 +1,31 @@
 
 public class Agent {
-
+	private int team;
+	private Location myCurrentLocation;
+	private Location initLocation;
+	private Location targetLocation;
+	
+	public Agent(int team, Location initLocation, Location targetLocation) {
+		super();
+		this.team = team;
+		this.initLocation = initLocation;
+		this.targetLocation = targetLocation;
+		this.myCurrentLocation = initLocation;
+	}
+	
+	public void up() {
+		
+	}
+	
+	public void donw() {
+		
+	}
+	public void left() {
+		
+	}
+	public void right() {
+		
+	}
+	
+	
 }
--- src/Map.java
+++ src/Map.java
@@ -1,6 +1,34 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Map {
-	Location[][] Grid;
 	
+	Location[][] grid;
+	private int width;
+	private int height;
+	
+	public Map(File input) {
+		super();
+		readInputFile(input);
+	}
+
+	private void readInputFile(File input) {
+		try {
+			FileReader in = new FileReader("C:/test.txt");
+		    BufferedReader br = new BufferedReader(in);
+			while (br.readLine() != null) {
+				System.out.println(br.readLine());
+		    	in.close();
+			}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	
 }
+			
